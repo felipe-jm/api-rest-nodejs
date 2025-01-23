2367ff3e6ff8e0aef9607454b2fa11f9b2944073
Felipe Jung <felipemattoseu@gmail.com>
2025-01-23 12:53:23 -0400

    Add knex and initials migrations

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import crypto from "node:crypto";
 
 const app = fastify();
 
-app.get("/", async (req, res) => {
-  const tables = await knex("sqlite_schema").select("*");
+app.get("/hello", async (req, res) => {
+  const tables = await knex("transactions").select("*");
+
   return tables;
 });
 
