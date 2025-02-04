3de01355097c363689dee7f421f541c48bd07fcb
Felipe Jung <felipemattoseu@gmail.com>
2025-02-04 12:44:51 -0400

    Add transactions tests

--- test/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "vitest";
-
-test("user can create a new transaction", () => {
-  // fazer chamada HTTP para a rota /transactions
-
-  const statusCode = 201;
-
-  expect(statusCode).toEqual(201);
-});
